99c357a709bee9e07605d348d031401637639c31
CesarAguirre <cesaraguirre566@gmail.com>
2018-06-16 21:21:12 -0300

    xml de loging y activiti main

---
+++ app/src/main/java/com/example/cesaraguirre/probandojson/Login.java
@@ -0,0 +1,18 @@
+package com.example.cesaraguirre.probandojson;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+    public void logearte (View view){
+        
+    }
+}
